f2d9d6f6a196647feb5781ddc1c1f2f6c6f41d12
ahamefulachibundu <ahamefulachibundu@gmail.com>
2023-11-14 02:56:03 -0800

    main.h file

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int _putchar(char c);
+int _print_str(char *str);
+
+#endif
