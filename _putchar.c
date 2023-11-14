63e2d3d78f6cca96b6490eab2f3db4ec1252fe2b
ahamefulachibundu <ahamefulachibundu@gmail.com>
2023-11-14 02:57:54 -0800

    prints characters

---
+++ _putchar.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+/**
+ * _putchar - writes the character c to stdout
+ *
+ * @c: The character to print using _putchar
+ *
+ * Return: 0
+ */
+
+int _putchar(char c)
+
+{
+
+	return (write(1, &c, 1));
+
+}
